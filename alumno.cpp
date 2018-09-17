3e59edbf51b5b38947abece0a7f51baaa662a464
chiptime <amor95_8@hotmail.com>
2018-09-17 22:41:23 +0200

    before select and delete/edit

--- alumno.cpp
+++ alumno.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define N 10
+#define N 3
 
 typedef struct{
     char nombre[30];
@@ -12,15 +13,14 @@ typedef struct{
 int menu(){
     int menuO;
     printf("Bienvenido,\nporfavor elige una de la opciones...\n\n");
-    printf("    0. Ver todos los alumnos\n");
-    printf("    1. Ver un alumno\n");
-    printf("    2. Insertar todos los alumnos\n");
-    printf("    3. Insertar un alumno\n");
-    printf("    4. Editar un alumno\n");
-    printf("    5. Borrar todos los alumnos\n");
-    printf("    6. Borrar un alumno\n");
-    printf("    7. Guardar los alumnos en el fichero\n");
-    printf("    8. Salir del programa\n");
+    printf("    0. Ver todos los alumnos\n");    
+    printf("    1. Insertar todos los alumnos\n");
+    printf("    2. Insertar un alumno\n");
+    printf("    3. Editar un alumno\n");
+    printf("    4. Borrar todos los alumnos\n");
+    printf("    5. Borrar un alumno\n");
+    printf("    6. Guardar los alumnos en el fichero\n");
+    printf("    7. Salir del programa\n");
     scanf("%i", &menuO);
     return menuO;
 }
@@ -31,39 +31,52 @@ Persona takePerson(){
     printf("Edad:\n");
     scanf("%hu", &persona.edad);
     printf("Telefono:\n");
-    scanf("%iu", &persona.telefono);
-    //printf("Los datos son: %s, %hu, %hu", persona.nombre,persona.edad,persona.telefono);
+    scanf("%iu", &persona.telefono);    
     return persona;
+}
+void read(Persona persona, FILE *pipe, const char *path){        
+    if(!(pipe = fopen(path,"r")))
+        fprintf(stderr,"No he encontrado el archivo.\n");    
+    while(fread(&persona, sizeof(Persona), 1, pipe)){
+        printf("%s,%hu,%iu\n", persona.nombre, persona.edad, persona.telefono);        
+    }
+    printf("\n\n");
+    fclose(pipe);    
 }
 void take(Persona lista[], int id){
     lista[id] = takePerson();
     printf("Los datos son: %s, %hu, %u", lista[id].nombre,lista[id].edad,lista[id].telefono);
     printf("\n\nGuardando... en tiempo de ejecucion\n\n");
 }
-void save(Persona *persona, FILE *pipe, const char *path){
+void save(Persona persona[N], FILE *pipe, const char *path){
     if(!(pipe = fopen(path,"wb")))
        fprintf(stderr,"No he encontrado el archivo.\n");
-    fwrite(persona,sizeof(Persona),1,pipe);
+    fwrite(persona,sizeof(Persona),N,pipe);
+    printf("Se ha guardado el alumnno:\n");
+    printf("%s,%hu,%iu\n", persona->nombre, persona->edad, persona->telefono);    
     fclose(pipe);
 }
 int selectId(int id){
     printf("Last ID = %i\n(0 is default)\n", id);
     printf("¿Que alumno deseas elegir?, dime su id\n");
     scanf("%i", &id);
+    if(id>=N){
+        printf("El id = %i no existe\n", id);
+    }
     return id;
 }
 int main(int argc, char const *argv[]){
     bool running = true;
     int option;
     enum {
-        viewAll,
-        viewOne,
+        viewAll,        
         insertAll,
         insertOne,
         edit,
         delAll,
         delOne,
         saveF,
+        list,
         exit
     };
     const char *filename = "a.txt";
@@ -71,6 +84,16 @@ int main(int argc, char const *argv[]){
     Persona lista[N];
     Persona alumnoLec;
     int id = 0;    
+    char nullStr[30] = {"\0"};
+    printf("Los alumnos hasta el momento son: \n\n");    
+    read(alumnoLec,file,filename);    
+    
+
+    if(!(file = fopen(filename,"r")))
+       fprintf(stderr,"No he encontrado el archivo.\n");
+    fread(lista,sizeof(Persona),N,file);
+    fclose(file);
+
 
     do{
         option = menu();
@@ -78,57 +101,66 @@ int main(int argc, char const *argv[]){
 
             case viewAll:
                 printf("    0. Has elegido VER todos los alumnos\n\n");
-                if(!(file = fopen(filename,"r")))
-                    fprintf(stderr,"No he encontrado el archivo.\n");    
-                while(fread(&alumnoLec, sizeof(Persona), N, file)){                
-                printf("%s,%hu,%iu\n", alumnoLec.nombre,alumnoLec.edad,alumnoLec.telefono);
-                }
-                fclose(file);
-            break;
-
-            case viewOne:
-                printf("    1. Has elegido VER un alumno\n\n");
-                if(!(file = fopen(filename,"r")))
-                    fprintf(stderr,"No he encontrado el archivo.\n");    
-                while(fread(&alumnoLec, sizeof(Persona), 1, file)){                
-                printf("%s,%hu,%iu\n", alumnoLec.nombre,alumnoLec.edad,alumnoLec.telefono);
-                }
-                fclose(file);
+                read(alumnoLec,file,filename);
             break;
 
             case insertAll:
-                printf("    2. Has elegido insertar todos los alumnos\n\n");
+                printf("    1. Has elegido insertar todos los alumnos\n\n");
                 for(id = 0; id<N; id++){
                 take(lista,id);
                 }
             break;
 
             case insertOne:
-                //printf("    1. Has elegido editar un alumno\n");
-                printf("    3. Has elegido insertar un alumno\n\n");
+                printf("    2. Has elegido insertar un alumno\n\n");
                 selectId(id);
+                if(&lista[id].nombre != NULL){
                 take(lista,id);
+                }else{
+                    printf("El id de alumno ya esta en uso\n");
+                }
             break;
             case edit:
-                printf("    4. Has elegido editar un alumno\n\n");                
+                printf("    3. Has elegido editar un alumno\n\n"); 
+                selectId(id);                
+                take(lista,id);               
             break;   
             case delAll:
-                printf("    5. Has elegido borrar un alumno\n\n");                
-                return EXIT_SUCCESS;
+                printf("    4. Has elegido borrar todos los alumnos\n\n"); 
+                for(int i =0;i<N;i++){
+                    strcpy( lista[i].nombre, nullStr);
+                    lista[i].edad     = '\0';
+                    lista[i].telefono = '\0';
+                }               
+                            //Se salta la pregunta de si/no
+                int sn; 
+                printf("¿Estas seguro que quieres borrar todos los alumnos?\n\n1(Si)/0(No)\n\n");               
+                scanf("%i", &sn);                
+                if(sn == 1){
+                    save(lista, file, filename);
+                    printf("La lista de alumnos ha sido borrada.\n\n");                    
+                }else{
+                    printf("Has cancelado el borrado de alumnos.\n\n");                    
+                }
             break;  
             case delOne:
-                printf("    6. Has elegido editar todos los alumnos\n\n");                
+                printf("    5. Has elegido editar un alumno\n\n");                
                 return EXIT_SUCCESS;
             break; 
-            case saveF:
-                //printf("    2. Has elegido borrar un alumno\n");
-                printf("    7.Has elegido guardar en el fichero\n\n");
-                save(lista, file, filename);
+            case saveF:                
+                printf("    6.Has elegido guardar en el fichero\n\n");                
+                save(lista, file, filename);                
                 printf("\n\nGuardando 2...\n\n");
             break;       
             case exit:
                 printf("    Has elegido salir del programa\n\n");                
                 return EXIT_SUCCESS;
+            break;
+            case list:
+                printf("    Estos alumnos estan guardados en el PROGRAMA\n\n");  
+                for(int i =0;i<N;i++){
+                    printf("%s, %i, %i \n", lista[i].nombre,lista[i].edad,lista[i].telefono);
+                }
             break;
             default:
                 printf("\n    Opción invalida, has sido expulsado\n\n");                
